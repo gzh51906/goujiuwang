75afdfcb7ce0a2a8477335b04a3a0549b5fe90e5
github <1527569484@qq.com>
2019-09-04 09:00:49 +0800

    xiong

--- src/App.vue
+++ src/App.vue
@@ -51,15 +51,6 @@
 </script>
 
 <style scoped>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  
   * {
     margin: 0;
     padding: 0;
@@ -73,6 +64,7 @@
     left: 0;
     right: 0;
     bottom: 0px;
+    z-index:100;
   }
   
   ul .tabbar {
