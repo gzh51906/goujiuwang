62f8a5ab2f08c7c480d8ac55fc4d5e3c2aceaa37
jike <2397724385qq@.com>
2019-09-10 13:51:55 +0800

    上传文件

--- src/components/benefits.vue
+++ src/components/benefits.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="height:700px;">
+  <div style="height:2240px;">
     <div class="header">
       <i class="el-icon-arrow-left" @click="gotoback"></i>
       <span>【购酒网】福利页面</span>
@@ -16,44 +16,42 @@
     <div class="pic">
       <img src="../assets/lp_04.jpg" alt style="width:100%;" />
     </div>
-     <div class="pic">
-      <img src="../assets/lp_05.jpg" alt style="width:187.5px; float:left"/>
-    </div>
-    <div class="pic">
-      <img src="../assets/lp_06.jpg" alt style="width:187.5px; float:left" />
-    </div>
+     <div class="pic" style="float:left">
+      <img src="../assets/lp_05.jpg" alt style="width:50%; float:left"/>
+      <img src="../assets/lp_06.jpg" alt style="width:50%; float:left" />
+     </div>
       <div class="pic">
       <img src="../assets/lp_07.jpg" alt style="width:100%;" />
     </div>
         <div class="pic">
-      <img src="../assets/lp_08.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_08.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_09.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_09.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_10.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_10.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_11.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_11.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_12.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_12.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_13.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_13.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_14.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_14.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_15.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_15.jpg" alt style="width:50%; float:left" />
     </div>
        <div class="pic">
-      <img src="../assets/lp_16.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_16.jpg" alt style="width:50%; float:left" />
     </div>
       <div class="pic">
-      <img src="../assets/lp_17.jpg" alt style="width:187.5px; float:left" />
+      <img src="../assets/lp_17.jpg" alt style="width:50%; float:left" />
     </div>
   </div>
 </template>
